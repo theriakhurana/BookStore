a23077c14279970ec7195885dbc53c27bac6b2ca
Ria Khurana <theriakhurana@gmail.com>
2023-11-28 23:14:39 +0530

    footer added, js

---
+++ try/items.js
@@ -0,0 +1,97 @@
+const items = [
+  {
+    id: '001',
+    image: 'images/adv_img1.jpg',
+    item_name: 'Treasure Hunt',
+    author_name: 'Peter Earle',
+    price: 299,
+    rating:{
+      stars: 4.5,
+      count: 1400,
+    }
+  },
+
+  {
+    id: '002',
+    image: 'images/adv_img2.jpg',
+    item_name: 'DownFall',
+    author_name: 'Nerys Wheatley',
+    price: 349,
+    rating:{
+      stars: 4.7,
+      count: 2450,
+    }
+  },
+
+  {
+    id: '003',
+    image: 'images/adv_img3.jpg',
+    item_name: 'Atlantis Found',
+    author_name: 'Clive Cussler',
+    price: 249,
+    rating:{
+      stars: 4.1,
+      count: 340,
+    }
+  },
+
+  {
+    id: '004',
+    image: 'images/adv_img4.jpg',
+    item_name: 'Voodoo Child',
+    author_name: 'William Burke',
+    price: 349,
+    rating:{
+      stars: 3.8,
+      count: 510,
+    }
+  },
+  
+  {
+    id: '005',
+    image: 'images/adv_img5.jpg',
+    item_name: 'The Mapmaker',
+    author_name: 'A.L. Tait',
+    price: 299,
+    rating:{
+      stars: 4.4,
+      count: 450,
+    }
+  },
+
+  {
+    id: '006',
+    image: 'images/adv_img6.jpg',
+    item_name: 'The Black Thunder',
+    author_name: 'Riley Peyton',
+    price: 199,
+    rating:{
+      stars: 3.5,
+      count: 240,
+    }
+  },
+
+  {
+    id: '007',
+    image: 'images/adv_img7.jpg',
+    item_name: 'The Call of Wild',
+    author_name: 'Jack London',
+    price: 299,
+    rating:{
+      stars: 4.6,
+      count: 280,
+    }
+  },
+
+  {
+    id: '008',
+    image: 'images/adv_img8.jpg',
+    item_name: 'Touching The Void',
+    author_name: 'Joe Simpson',
+    price: 249,
+    rating:{
+      stars: 4.7,
+      count: 630,
+    }
+  },
+]
